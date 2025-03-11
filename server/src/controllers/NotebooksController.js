219e7b8df78093599c0d304475542e37fcd7de13
heather bickish <hbickish@gmail.com>
2025-03-11 16:43:25 -0600

    getting my notebooks and getting by id

--- server/src/controllers/NotebooksController.js
+++ server/src/controllers/NotebooksController.js
@@ -6,8 +6,11 @@ export class NotebooksController extends BaseController {
   constructor() {
     super('api/notebooks')
     this.router
+      .get('/:notebookId', this.getNotebookById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createNotebook)
+      .get('', this.getAllMyNotebooks)
+      .put('/:notebookId', this.editNotebook)
   }
 
 
@@ -22,4 +25,35 @@ export class NotebooksController extends BaseController {
     }
   }
 
+  async getAllMyNotebooks(request, response, next) {
+    try {
+      const userId = request.userInfo.id
+      const notebooks = await notebooksService.getAllMyNotebooks(userId)
+      response.send(notebooks)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getNotebookById(request, response, next) {
+    try {
+      const notebookId = request.params.notebookId
+      const notebook = await notebooksService.getNotebookById(notebookId)
+      response.send(notebook)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editNotebook(request, response, next) {
+    try {
+      const notebookId = request.params.notebookId
+      const updateData = request.body
+      const userId = request.userInfo.id
+      const updatedNotebook = await notebooksService.editNotebook(notebookId, userId, updateData)
+      response.send(updatedNotebook)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
--- server/src/services/NotebooksService.js
+++ server/src/services/NotebooksService.js
@@ -1,9 +1,31 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 class NotebooksService {
+  async editNotebook(notebookId, userId, updateData) {
+    const originalNotebook = await dbContext.NoteBooks.findById(notebookId)
+
+    if (!originalNotebook) { throw new Error(`Invalid notebook id: ${notebookId}`) }
+    if (userId != originalNotebook.creatorId) { throw new Forbidden("YOU CANT EDIT THIS NOTEBOOK IT DOESNT BELONG TO YOU, BRAH") }
+    if (updateData.title) originalNotebook.title = updateData.title
+    if (updateData.icon) originalNotebook.icon = updateData.icon
+    if (updateData.color) originalNotebook.color = updateData.color
+    if (updateData.coverImg) originalNotebook.coverImg = updateData.coverImg
+
+    await originalNotebook.save()
+    return originalNotebook
+  }
+  async getNotebookById(notebookId) {
+    const notebook = await dbContext.NoteBooks.findById(notebookId).populate('creator', 'name picture')
+    return notebook
+  }
+  async getAllMyNotebooks(userId) {
+    const notebooks = await dbContext.NoteBooks.find({ creatorId: userId })
+    return notebooks
+  }
   async createNotebook(notebookData) {
     const notebook = await dbContext.NoteBooks.create(notebookData)
-    await notebook.populate('creator')
+    await notebook.populate('creator', 'name picture')
     return notebook
   }
 
