dfdd0d51a90b55530d47702d36c737ebd13d7853
heather bickish <hbickish@gmail.com>
2025-03-12 13:43:27 -0600

    create notebook form

--- client/src/components/OffCanvas.vue
+++ client/src/components/OffCanvas.vue
@@ -4,30 +4,36 @@
 
 
 <template>
-
-  <!-- <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-  Button with data-bs-target
-</button> -->
-
   <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvas" aria-labelledby="offcanvas">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvas">Offcanvas</h5>
+      <h5 class="offcanvas-title" id="offcanvas"><em><b>Manage Notebooks</b></em></h5>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
       <div>
-        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists,
-        etc.
-      </div>
-      <div class="dropdown mt-3">
-        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-          Dropdown button
-        </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
+        <form>
+          <div class="d-flex gap-2">
+            <div class="mb-3">
+              <label class="form-label"><em><b>Notebook Title</b></em></label>
+              <input type="text" class="form-control" id="title">
+            </div>
+            <div class="mb-3">
+              <label class="form-label"><em><b>Icon</b></em></label>
+              <input type="text" class="form-control" id="icon">
+            </div>
+            <div class="mb-3">
+              <label class="form-label"><em><b>Color</b></em></label>
+              <input type="color" class="form-control h-50" id="color">
+            </div>
+          </div>
+          <div class="mb-3">
+            <label class="form-label"><em><b>Cover Image</b></em></label>
+            <input type="url" class="form-control" id="coverImg">
+          </div>
+          <div class="text-end">
+            <button class="btn btn-danger"><em><b>Create Notebook</b></em></button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
