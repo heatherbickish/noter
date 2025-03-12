dcef4aa8b25317009fae4eae194e72485b3ddcb9
heather bickish <hbickish@gmail.com>
2025-03-12 16:13:37 -0600

    displaying notebook details on card

--- client/src/pages/NotebookDetailsPage.vue
+++ client/src/pages/NotebookDetailsPage.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { AppState } from "@/AppState.js";
 import { notesbooksService } from "@/services/NotebooksService.js";
 import { logger } from "@/utils/Logger.js";
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
 
 
 const route = useRoute()
+const account = computed(() => AppState.account)
+const notebook = computed(() => AppState.activeNotebook)
 
 onMounted(() => {
   getNotebookById()
@@ -24,39 +27,41 @@ async function getNotebookById() {
 
 
 <template>
-  <section class="container-fluid">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card shadow">
-          <div class="card-img">
-            <img
-              src="https://images.unsplash.com/photo-1559906727-76b9259eb4e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt="" class="notebook-img">
-          </div>
-          <div class="card-body bg-dark text-light">
-            <div class="d-flex align-items-baseline justify-content-between">
-              <p>Created by shellcat</p>
-              <h4>Skyrim and her people</h4>
+  <div v-if="notebook">
+    <section class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card shadow">
+            <div class="card-img">
+              <img :src="notebook.coverImg" alt="" class="notebook-img">
             </div>
-            <div class="flex-container">
-              <div class="">
-                <p>Created 08/23/22</p>
-                <p>last updated 08/30/22</p>
+            <div class="card-body bg-dark text-light">
+              <div class="d-flex align-items-baseline justify-content-between">
+                <p>Created by {{ notebook.creator.name }}</p>
+                <h4>{{ notebook.title }}</h4>
               </div>
-              <div class="text-end">
-                <p>13 Entries</p>
-                <button class="btn btn-outline-light px-5 text-light me-2">Edit</button>
-                <button class="btn btn-outline-light px-5 text-light me-2">Delete</button>
-                <button class="btn btn-primary px-5 text-light">
-                  <i class="mdi mdi-plus-box-outline me-1"></i>New
-                </button>
+              <div class="flex-container">
+                <div class="">
+                  <p>Created {{ notebook.createdAt.toLocaleDateString() }}</p>
+                  <p>Last updated {{ notebook.updatedAt.toLocaleDateString() }}</p>
+                </div>
+                <div class="text-end">
+                  <p>{{ notebook.entryCount }} Entries</p>
+                  <div v-if="notebook.creatorId == account?.id">
+                    <button class="btn btn-outline-light px-5 text-light me-2">Edit</button>
+                    <button class="btn btn-outline-light px-5 text-light me-2">Delete</button>
+                    <button class="btn btn-primary px-5 text-light">
+                      <i class="mdi mdi-plus-box-outline me-1"></i>New
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 
